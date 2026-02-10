afe46e036d35f3fa7c21826d1135eb8ad2d81416
Johnny17V <jjasenghor@ept.sn>
2026-02-10 18:16:27 +0000

    Creation model de projet

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+class Tags(models.Model):
+    name = models.CharField(max_length=50)
+    icon_class = models.CharField(max_length = 50, blank = True, help_text="Class Font Awesome")
+    
+    def __str__(self):
+        return f"{self.name}"
+    
+    class Meta:
+        verbose_name = 'Tag'
+        verbose_name_plural = 'Tags'
+    
+
+class ProjetModel(models.Model):
+    Description = models.TextField()
+    titre = models.CharField(max_length=100)
+    lienGitHub = models.URLField(max_length=300, blank=True)
+    imageProjet = models.ImageField(upload_to= "api/images/", blank=True, null=True)
+    rapportPDF = models.FileField(upload_to = "api/rapports/", blank = True, null = True)
+    lesTags = models.ManyToManyField(Tags, related_name='projets')
+    
+    def __str__(self):
+        return f"{self.titre}"
+    
+    class Meta:
+        verbose_name = 'Projet'
+        verbose_name_plural = 'Projets'
+    
