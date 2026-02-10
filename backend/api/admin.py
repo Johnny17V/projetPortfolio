8c194a7df3903eefe5fad3d864c68f863c018ec2
Johnny17V <jjasenghor@ept.sn>
2026-02-10 23:43:14 +0000

    Création des différents modéles

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Tags, ProjetModel, ReseauSociaux, Experience, Skill
 # Register your models here.
+admin.site.register(Tags)
+admin.site.register(ProjetModel)
+admin.site.register(ReseauSociaux)
+admin.site.register(Experience)
+admin.site.register(Skill)
+
