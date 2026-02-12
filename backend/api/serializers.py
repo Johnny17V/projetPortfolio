508e3740f16f97c01e110b74054bc34737150c47
Johnny17V <jjasenghor@ept.sn>
2026-02-12 19:11:52 +0000

    Ajout des différents serializers

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,31 +1,31 @@
 from rest_framework import serializers
 from .models import Tags, ProjetModel, ReseauSociaux, Experience, Skill
 
-class TagsSerialiser(serializers.Serializer):
+class TagsSerialiser(serializers.ModelSerializer):
     
     class Meta:
         model = Tags
         fields = "__all__"
         
-class ProjetModelSerialiser(serializers.Serializer):
+class ProjetModelSerialiser(serializers.ModelSerializer):
     
     class Meta:
         model = ProjetModel
         fields = "__all__"
 
-class ReseauSociauxSerialiser(serializers.Serializer):
+class ReseauSociauxSerialiser(serializers.ModelSerializer):
     
     class Meta:
         model = ReseauSociaux
         fields = "__all__"
         
-class ExperienceSerialiser(serializers.Serializer):
+class ExperienceSerialiser(serializers.ModelSerializer):
     
     class Meta:
         model = Experience
         fields = "__all__"
         
-class SkillSerialiser(serializers.Serializer):
+class SkillSerialiser(serializers.ModelSerializer):
     
     class Meta:
         model = Skill
